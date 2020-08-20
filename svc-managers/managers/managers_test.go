ed686970d5e5973364531ed6dfc6fb5205f08b46
Arjun Ajith <arjun.ajith@hpe.com>
2020-08-20 17:25:15 +0530

    removed comments

--- svc-managers/managers/managers_test.go
+++ svc-managers/managers/managers_test.go
@@ -141,14 +141,8 @@ func TestGetManagerInvalidID(t *testing.T) {
 }
 
 func TestGetManagerResourcewithBadManagerID(t *testing.T) {
+
 	config.SetUpMockConfig(t)
-/*	defer func() {
-		err := common.TruncateDB(common.InMemory)
-		if err != nil {
-			t.Fatalf("error: %v", err)
-		}
-	}()
-*/
 	req := &managersproto.ManagerRequest{
 		ManagerID: "invalidURL",
 		URL:       "/redfish/v1/Managers/uuid",

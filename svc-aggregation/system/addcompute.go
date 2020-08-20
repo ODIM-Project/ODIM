c064a1e7630c78a99472944d1186ff12812b1e32
shiva-charan-m-s <shiva-charan.m-s@hpe.com>
2020-08-20 09:37:31 +0000

    populating odim with firmware and software inventory during add compute

--- svc-aggregation/system/addcompute.go
+++ svc-aggregation/system/addcompute.go
@@ -256,6 +256,32 @@ func (e *ExternalInterface) addCompute(taskID, targetURI string, percentComplete
 	task = fillTaskData(taskID, targetURI, resp, common.Running, common.OK, percentComplete, http.MethodPost)
 	e.UpdateTask(task)
 
+	// Populate the resource Firmware inventory for update service
+	pluginContactRequest.DeviceInfo = getSystemBody
+	pluginContactRequest.OID = "/redfish/v1/UpdateService/FirmwareInventory"
+	pluginContactRequest.DeviceUUID = saveSystem.DeviceUUID
+	pluginContactRequest.HTTPMethodType = http.MethodGet
+
+	progress = percentComplete
+	firmwareEstimatedWork := int32(15)
+	progress = h.getAllRootInfo(taskID, progress, firmwareEstimatedWork, pluginContactRequest)
+	percentComplete = progress
+	task = fillTaskData(taskID, targetURI, resp, common.Running, common.OK, percentComplete, http.MethodPost)
+	e.UpdateTask(task)
+
+	// Populate the resource Software inventory for update service
+	pluginContactRequest.DeviceInfo = getSystemBody
+	pluginContactRequest.OID = "/redfish/v1/UpdateService/SoftwareInventory"
+	pluginContactRequest.DeviceUUID = saveSystem.DeviceUUID
+	pluginContactRequest.HTTPMethodType = http.MethodGet
+
+	progress = percentComplete
+	softwareEstimatedWork := int32(15)
+	progress = h.getAllRootInfo(taskID, progress, softwareEstimatedWork, pluginContactRequest)
+	percentComplete = progress
+	task = fillTaskData(taskID, targetURI, resp, common.Running, common.OK, percentComplete, http.MethodPost)
+	e.UpdateTask(task)
+
 	// Lets Discover/gather registry files of this server and store them in DB
 
 	pluginContactRequest.DeviceInfo = getSystemBody
@@ -290,7 +316,7 @@ func (e *ExternalInterface) addCompute(taskID, targetURI string, percentComplete
 
 	progress = percentComplete
 	chassisEstimatedWork := int32(15)
-	progress = h.getAllChassisInfo(taskID, progress, chassisEstimatedWork, pluginContactRequest)
+	progress = h.getAllRootInfo(taskID, progress, chassisEstimatedWork, pluginContactRequest)
 	percentComplete = progress
 	task = fillTaskData(taskID, targetURI, resp, common.Running, common.OK, percentComplete, http.MethodPost)
 	err = e.UpdateTask(task)

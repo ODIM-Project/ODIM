e24fffd8cabdb30dee16bd928144eaea3dd47cbf
Arjun Ajith <arjun.ajith@hpe.com>
2020-10-14 15:01:52 +0530

    corrected the error message as per the review comments

--- lib-utilities/services/auth.go
+++ lib-utilities/services/auth.go
@@ -43,7 +43,7 @@ func IsAuthorized(sessionToken string, privileges, oemPrivileges []string) errRe
 		},
 	)
 	if err != nil && response == nil {
-		errMsg := fmt.Sprintf("error: something went wrong with rpc call: %v", err)
+		errMsg := fmt.Sprintf("error: rpc call failed: %v", err)
 		log.Println(errMsg)
 		return common.GeneralError(http.StatusInternalServerError, errResponse.InternalError, errMsg, nil, nil)
 	}

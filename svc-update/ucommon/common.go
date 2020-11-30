e72efffeda7f9bc66cf106c8eed912640d106639
Bharath Kumar Bhaskar Kizhur <68339187+Bharath-KKB@users.noreply.github.com>
2020-11-30 15:41:51 +0530

    Update common.go

--- svc-update/ucommon/common.go
+++ svc-update/ucommon/common.go
@@ -269,10 +269,10 @@ func getPluginStatus(plugin umodel.Plugin) bool {
 	}
 	status, _, _, err := pluginStatus.CheckStatus()
 	if err != nil && !status {
-		log.Warn("Error While getting the status for plugin ", plugin.ID, err)
+		log.Warn("Error While getting the status for plugin " + plugin.ID + " " + err)
 		return status
 	}
-	log.Info("Status of plugin", plugin.ID, status)
+	log.Info("Status of plugin " + plugin.ID + " " + status)
 	return status
 }
 

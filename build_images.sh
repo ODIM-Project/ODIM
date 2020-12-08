9ac9b570761a0652154f3b82416c0b3b332a9214
Arjun Ajith <arjun.ajith@hpe.com>
2020-12-08 15:22:04 +0530

    added docker file for odim build

--- build_images.sh
+++ build_images.sh
@@ -31,18 +31,18 @@ else
                     --build-arg ODIMRA_GROUP_ID=${ODIMRA_GROUP_ID}"
 fi
 
-
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.accountSession -t account-session:1.0 $build_args .
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.aggregation -t aggregation:1.0 $build_args .
+/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.odim -t odim:1.0 .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.accountSession -t account-session:1.0 $build_args .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.aggregation -t aggregation:1.0 $build_args .
 /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.api -t api:1.0 $build_args .
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.consul -t consul:1.6 $build_args .
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.events -t events:1.0 $build_args .
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.fabrics -t fabrics:1.0 $build_args .
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.grfplugin -t grf-plugin:1.0 $build_args .
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.kafka -t odim_kafka:1.0 $build_args .
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.managers -t managers:1.0 $build_args .
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.redis -t redis:1.0 $build_args .
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.systems -t systems:1.0 $build_args .
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.task -t task:1.0 $build_args .
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.update -t update:1.0 $build_args .
-/usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.zookeeper -t odim_zookeeper:1.0 $build_args .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.consul -t consul:1.6 $build_args .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.events -t events:1.0 $build_args .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.fabrics -t fabrics:1.0 $build_args .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.grfplugin -t grf-plugin:1.0 $build_args .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.kafka -t odim_kafka:1.0 $build_args .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.managers -t managers:1.0 $build_args .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.redis -t redis:1.0 $build_args .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.systems -t systems:1.0 $build_args .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.task -t task:1.0 $build_args .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.update -t update:1.0 $build_args .
+# /usr/bin/docker build -f install/Docker/dockerfiles/Dockerfile.zookeeper -t odim_zookeeper:1.0 $build_args .

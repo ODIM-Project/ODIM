d710010b89887ec6cf71a0bac6bcad936fbd9eb1
Bharath Kumar Bhaskar Kizhur <68339187+Bharath-KKB@users.noreply.github.com>
2020-12-17 21:42:43 +0530

    Update main.go

--- svc-aggregation/main.go
+++ svc-aggregation/main.go
@@ -45,15 +45,15 @@ func main() {
 		log.Fatal("Aggregation Service should not be run as the root user")
 	}
 	if err := config.SetConfiguration(); err != nil {
-		log.Fatalf("error while trying to set configuration: %v", err)
+		log.Fatal("error while trying to set configuration: " + err.Error())
 	}
 
 	if err := dc.SetConfiguration(config.Data.MessageQueueConfigFilePath); err != nil {
-		log.Fatalf("error while trying to set messagebus configuration: %v", err)
+		log.Fatal("error while trying to set messagebus configuration: " + err.Error())
 	}
 
 	if err := common.CheckDBConnection(); err != nil {
-		log.Fatalf("error while trying to check DB connection health: %v", err)
+		log.Fatalf("error while trying to check DB connection health: ", err.Error())
 	}
 
 	var connectionMethoodInterface = agcommon.DBInterface{
@@ -63,12 +63,12 @@ func main() {
 		DeleteInterface:              agmodel.Delete,
 	}
 	if err := connectionMethoodInterface.AddConnectionMethods(config.Data.ConnectionMethodConf); err != nil {
-		log.Fatalf("error while trying add connection method: %v", err)
+		log.Fatal("error while trying add connection method: " + err.Error())
 	}
 
 	err := services.InitializeService(services.Aggregator)
 	if err != nil {
-		log.Fatalf("fatal: error while trying to initialize service: %v", err)
+		log.Fatal("fatal: error while trying to initialize service: " + err.Error())
 	}
 
 	aggregator := rpc.GetAggregator()
@@ -88,11 +88,11 @@ func main() {
 	go p.RediscoverResources()
 	agcommon.ConfigFilePath = os.Getenv("CONFIG_FILE_PATH")
 	if agcommon.ConfigFilePath == "" {
-		log.Fatalln("error: no value get the environment variable CONFIG_FILE_PATH")
+		log.Fatal("error: no value get the environment variable CONFIG_FILE_PATH")
 	}
 	go agcommon.TrackConfigFileChanges(connectionMethoodInterface)
 	if err = services.Service.Run(); err != nil {
-		log.Fatalf("failed to run a service: %v", err)
+		log.Fatal("failed to run a service: " + err.Error())
 	}
 
 }

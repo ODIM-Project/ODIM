f5a3ae13f5bd0383149c678155158c04c4a1ee4c
Amar <amar2@hpe.com>
2021-01-22 15:55:12 +0530

    Removing the build files as these are not required

--- plugin-unmanaged-racks/build/build.sh
+++
@@ -1,8 +0,0 @@
-go mod download
-go mod vendor
-go build .
-if [ $? -eq 0 ]; then
-    echo Build for plugin ur service $i is Successful !!!!
-else
-    echo Build for plugin ur service $i is Failed !!!!
-fi
--- plugin-unmanaged-racks/build/command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cp -r /var/ur_plugin_config/ /etc/ && rm -rf /var/ur_plugin_config/* && /ur-plugin/start_plugin.sh

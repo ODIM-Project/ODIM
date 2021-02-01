5f37f3261a4d6042bdab96e8b709483e3dccaa2b
Bharath B <bharath.b@hpe.com>
2021-02-01 19:02:07 +0000

    issue-375 : monitor and exit when odim-ra process is not running

--- install/Docker/dockerfiles/scripts/start_grfplugin.sh
+++ install/Docker/dockerfiles/scripts/start_grfplugin.sh
@@ -14,12 +14,12 @@
 # under the License.
 
 declare PID=0
+declare OWN_PID=$$
 
 sigterm_handler()
 {
         if [[ $PID -ne 0 ]]; then
-                # will wait for other instances to gracefully announce quorum exit
-                sleep 5
+                sleep 1
                 kill -9 $PID
                 wait "$PID" 2>/dev/null
         fi
@@ -40,20 +40,21 @@ run_forever()
 
 start_grfplugin()
 {
-	cd /bin
 	export PLUGIN_CONFIG_FILE_PATH=/etc/grfplugin_config/config.json
-	nohup ./plugin-redfish >> /var/log/grfplugin_logs/grfplugin.log 2>&1 &
+	nohup /bin/plugin-redfish >> /var/log/grfplugin_logs/grfplugin.log 2>&1 &
 	PID=$!
-	sleep 2s
-  nohup /bin/add-hosts -file /tmp/host.append >> /var/log/grfplugin_logs/add-hosts.log 2>&1 &
+	sleep 3
+
+	nohup /bin/add-hosts -file /tmp/host.append >> /var/log/grfplugin_logs/add-hosts.log 2>&1 &
 }
 
 monitor_process()
 {
         while true; do
-                pid=$(pgrep plugin-redfish 2> /dev/null)
-                if [[ $pid -eq 0 ]]; then
+                pid=$(pgrep -fc plugin-redfish 2> /dev/null)
+                if [[ $? -ne 0 ]] || [[ $pid -gt 1 ]]; then
                         echo "plugin-redfish has exited" >> /var/log/grfplugin_logs/grfplugin.log 2>&1 &
+			kill -15 ${OWN_PID}
                         exit 1
                 fi
                 sleep 5

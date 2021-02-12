1100b97a966f7ac9ea17712d5a3914c5ef538ca6
Arjun Ajith <arjun.ajith@hpe.com>
2021-02-12 13:06:08 +0530

    added more information in the comment

--- svc-systems/plugin/client.go
+++ svc-systems/plugin/client.go
@@ -152,8 +152,11 @@ func (m *multiTargetClient) Get(uri string, opts ...CallOption) response.RPC {
 			log.Printf("execution of GET %s on %s plugin returned non 2xx status code; %v", uri, target.ID, resp.Body)
 		}
 	}
+	// Checking whether the struct passed as the interface has a reqURI field.
+	// If it is a collection request then the ReqURI field won't be present.
+	// Only struct associating with individual resource collection requires the reqURI field.
 	field := reflect.ValueOf(m.call.collector).Elem().FieldByName("ReqURI")
-	if field.IsValid() { // checking whether the struct passed as interface has ReqURI field
+	if field.IsValid() {
 		chassisID := getChassisID(uri)
 		reflect.ValueOf(m.call.collector).Elem().FieldByName("ReqURI").SetString(chassisID)
 	}

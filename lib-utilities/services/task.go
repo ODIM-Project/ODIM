58ebc302d65da5550d62becd578c9e3d92f2d6fe
lakkundipooja <pooja-p.lakkundi@hpe.com>
2021-02-15 23:47:47 -0800

    addressing comments

--- lib-utilities/services/task.go
+++ lib-utilities/services/task.go
@@ -34,7 +34,7 @@ func CreateTask(sessionUserName string) (string, error) {
 		},
 	)
 	if err != nil && response == nil {
-		log.Error("Something went wrong with rpc call: " + err.Error())
+		log.Error("rpc error: " + err.Error())
 		return "", err
 	}
 	return response.TaskURI, err
@@ -51,7 +51,7 @@ func CreateChildTask(sessionUserName string, parentTaskID string) (string, error
 		},
 	)
 	if err != nil && response == nil {
-		log.Error("Something went wrong with rpc call: " + err.Error())
+		log.Error("rpc error: " + err.Error())
 		return "", err
 	}
 	return response.TaskURI, err

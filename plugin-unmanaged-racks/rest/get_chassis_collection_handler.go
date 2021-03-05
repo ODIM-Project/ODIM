6351399e9a859f7388b6a7c2ded9f766a4ca01de
Bharath Kumar Bhaskar Kizhur <68339187+Bharath-KKB@users.noreply.github.com>
2021-03-05 14:28:05 +0530

    Update get_chassis_collection_handler.go

--- plugin-unmanaged-racks/rest/get_chassis_collection_handler.go
+++ plugin-unmanaged-racks/rest/get_chassis_collection_handler.go
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020 Intel Corporation
+ * (C) Copyright [2021] Hewlett Packard Enterprise Development LP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

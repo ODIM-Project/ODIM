f3177c020a0c5383219df3ad6e1a43434a553ed6
Bharath Kumar Bhaskar Kizhur <68339187+Bharath-KKB@users.noreply.github.com>
2021-03-05 14:28:37 +0530

    Update patch_chassis_handler.go

--- plugin-unmanaged-racks/rest/patch_chassis_handler.go
+++ plugin-unmanaged-racks/rest/patch_chassis_handler.go
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020 Intel Corporation
+ * (C) Copyright [2020] Hewlett Packard Enterprise Development LP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

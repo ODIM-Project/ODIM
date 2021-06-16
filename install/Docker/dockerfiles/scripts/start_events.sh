bfae6b8b95cec6176375b7a9126519fefffe1b84
Anjum <syeda-anjum.khan@hpe.com>
2021-06-16 15:26:12 +0530

    grpc changes for events

--- install/Docker/dockerfiles/scripts/start_events.sh
+++ install/Docker/dockerfiles/scripts/start_events.sh
@@ -40,9 +40,9 @@ run_forever()
 
 start_event()
 {
-        registry_address="consul:8500"
+        registry_address="etcd:2379"
 	export CONFIG_FILE_PATH=/etc/odimra_config/odimra_config.json
-	nohup /bin/svc-events --registry=consul --registry_address=${registry_address} --server_address=events:45103 --client_request_timeout=`expr $(cat $CONFIG_FILE_PATH | grep SouthBoundRequestTimeoutInSecs | cut -d : -f2 | cut -d , -f1 | tr -d " ")`s >> /var/log/odimra_logs/events.log 2>&1 &
+	nohup /bin/svc-events --registry=etcd --registry_address=${registry_address} --server_address=events:45103 --framework=GRPC --client_request_timeout=`expr $(cat $CONFIG_FILE_PATH | grep SouthBoundRequestTimeoutInSecs | cut -d : -f2 | cut -d , -f1 | tr -d " ")`s >> /var/log/odimra_logs/events.log 2>&1 &
 	PID=$!
 	sleep 3
 

8cb9ed7cd41bfa438667973073460b24f64adb7b
Bharath B <bharath.b@hpe.com>
2021-06-18 11:34:35 +0000

    issue-567 : plugins should maintain the inventory of managed servers

--- plugin-redfish/rfphandler/pluginStatus.go
+++ plugin-redfish/rfphandler/pluginStatus.go
@@ -87,7 +87,7 @@ func GetPluginStartup(ctx iris.Context) {
 		}
 	}
 
-	var startup []rfpmodel.Startup
+	var startup rfpmodel.StartUpData
 	err := ctx.ReadJSON(&startup)
 	if err != nil {
 		errMsg := "Unable to collect data from request: " + err.Error()
@@ -96,6 +96,13 @@ func GetPluginStartup(ctx iris.Context) {
 		ctx.WriteString(errMsg)
 		return
 	}
+
+	if len(startup.Devices) <= 0 {
+		log.Info("startup devices list is empty")
+		ctx.StatusCode(http.StatusOK)
+		return
+	}
+
 	errorCh := make(chan error)
 	startUpResponse := make(chan map[string]string)
 	respBody := make(map[string]string)
@@ -126,11 +133,20 @@ func GetPluginStartup(ctx iris.Context) {
 			}
 		}
 	}()
-	for _, server := range startup {
-		writeWG.Add(1)
-		go checkCreateSub(server, startUpResponse, errorCh, &writeWG)
-		//go checkCreateSub(server, startUpResponse, respHeader, errorCh)
+
+	for uuid, device := range startup.Devices {
+		if device.Operation == "add" {
+			rfpmodel.AddDeviceToInventory(uuid, device)
+		}
+		if device.Operation == "del" {
+			rfpmodel.DeleteDeviceInInventory(uuid)
+		}
+		if startup.ResyncEvtSubscription && startup.RequestType == "full" {
+			writeWG.Add(1)
+			go checkCreateSub(device, startUpResponse, errorCh, &writeWG)
+		}
 	}
+
 	writeWG.Wait()
 	quit <- true
 	ctx.StatusCode(http.StatusOK)
@@ -138,14 +154,14 @@ func GetPluginStartup(ctx iris.Context) {
 	return
 }
 
-func checkCreateSub(startup rfpmodel.Startup, startUpResponse chan map[string]string, errorCh chan error, writeWG *sync.WaitGroup) {
+func checkCreateSub(server rfpmodel.DeviceData, startUpResponse chan map[string]string, errorCh chan error, writeWG *sync.WaitGroup) {
 	var respBody = make(map[string]string)
 
 	device := &rfputilities.RedfishDevice{
-		Host:     startup.Device.Host,
-		Username: startup.Device.Username,
-		Password: string(startup.Device.Password),
-		Location: startup.Location,
+		Host:     server.Address,
+		Username: server.UserName,
+		Password: string(server.Password),
+		Location: server.EventSubscriptionInfo.Location,
 	}
 	redfishClient, err := rfputilities.GetRedfishClient()
 	if err != nil {
@@ -172,7 +188,7 @@ func checkCreateSub(startup rfpmodel.Startup, startUpResponse chan map[string]st
 			return
 		}
 
-		res := reflect.DeepEqual(obj.EventTypes, startup.EventTypes)
+		res := reflect.DeepEqual(obj.EventTypes, server.EventSubscriptionInfo.EventTypes)
 		if !res {
 			//Delete Subscription details
 			resp, err := redfishClient.DeleteSubscriptionDetail(device)
@@ -185,7 +201,7 @@ func checkCreateSub(startup rfpmodel.Startup, startUpResponse chan map[string]st
 			//Create new Subscription with details in odimra
 			req := rfpmodel.EvtSubPost{
 				Destination: "https://" + pluginConfig.Data.LoadBalancerConf.Host + ":" + pluginConfig.Data.LoadBalancerConf.Port + pluginConfig.Data.EventConf.DestURI,
-				EventTypes:  startup.EventTypes,
+				EventTypes:  server.EventSubscriptionInfo.EventTypes,
 				Context:     "Event Subscription",
 				//      HTTPHeaders: reqPostBody.HTTPHeaders,
 				Protocol: "Redfish",
@@ -229,7 +245,7 @@ func checkCreateSub(startup rfpmodel.Startup, startUpResponse chan map[string]st
 		defer resp.Body.Close()
 	}
 
-	respBody[startup.Device.Host] = resp.Header.Get("location")
+	respBody[device.Host] = resp.Header.Get("location")
 	startUpResponse <- respBody
 	return
 }

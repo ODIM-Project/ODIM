30edc512f0bbf910ecde6cf7d504c78e0e05f294
jeevan-kamkar <jeevan.kamkar@hpe.com>
2021-08-17 23:18:46 +0530

    issue 523:Removing framework type parma from commandline args

--- install/Docker/dockerfiles/scripts/start_account_session.sh
+++ install/Docker/dockerfiles/scripts/start_account_session.sh
@@ -42,7 +42,7 @@ start_account_session()
 {
 	registry_address="etcd:2379"
 	export CONFIG_FILE_PATH=/etc/odimra_config/odimra_config.json
-	nohup /bin/svc-account-session --registry=etcd --registry_address=${registry_address} --server_address=account-session:45101 =GRPC  --client_request_timeout=`expr $(cat $CONFIG_FILE_PATH | grep SouthBoundRequestTimeoutInSecs | cut -d : -f2 | cut -d , -f1 | tr -d " ")`s >> /var/log/odimra_logs/account_session.log 2>&1 &
+	nohup /bin/svc-account-session --registry=etcd --registry_address=${registry_address} --server_address=account-session:45101   --client_request_timeout=`expr $(cat $CONFIG_FILE_PATH | grep SouthBoundRequestTimeoutInSecs | cut -d : -f2 | cut -d , -f1 | tr -d " ")`s >> /var/log/odimra_logs/account_session.log 2>&1 &
 	PID=$!
 	sleep 3
 
--- install/Docker/dockerfiles/scripts/start_aggregation.sh
+++ install/Docker/dockerfiles/scripts/start_aggregation.sh
@@ -42,7 +42,7 @@ start_aggregation()
 {
         registry_address="etcd:2379"
 	export CONFIG_FILE_PATH=/etc/odimra_config/odimra_config.json
-	nohup /bin/svc-aggregation =GRPC --registry=etcd --registry_address=${registry_address} --server_address=aggregation:45102 --client_request_timeout=`expr $(cat $CONFIG_FILE_PATH | grep SouthBoundRequestTimeoutInSecs | cut -d : -f2 | cut -d , -f1 | tr -d " ")`s >> /var/log/odimra_logs/aggregation.log 2>&1 &
+	nohup /bin/svc-aggregation  --registry=etcd --registry_address=${registry_address} --server_address=aggregation:45102 --client_request_timeout=`expr $(cat $CONFIG_FILE_PATH | grep SouthBoundRequestTimeoutInSecs | cut -d : -f2 | cut -d , -f1 | tr -d " ")`s >> /var/log/odimra_logs/aggregation.log 2>&1 &
 	PID=$!
 	sleep 3
 

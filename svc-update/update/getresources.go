eff18701cc93054284035e309f9bb46e6aa1030a
Shiva Charan M S <shiva-charan.m-s@hpe.com>
2021-09-07 14:15:54 +0530

    update service action response fix (#39)

--- svc-update/update/getresources.go
+++ svc-update/update/getresources.go
@@ -93,10 +93,10 @@ func (e *ExternalInterface) GetUpdateService() response.RPC {
 		},
 		Actions: uresponse.Actions{
 			UpdateServiceSimpleUpdate: uresponse.UpdateServiceSimpleUpdate{
-				Target: "/redfish/v1/UpdateService/Actions/SimpleUpdate",
+				Target: "/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate",
 			},
 			UpdateServiceStartUpdate: uresponse.UpdateServiceStartUpdate{
-				Target: "/redfish/v1/UpdateService/Actions/StartUpdate",
+				Target: "/redfish/v1/UpdateService/Actions/UpdateService.StartUpdate",
 			},
 		},
 	}

1fef5098c85efca831a64a33185e547990bc1372
hemkapoor <hemant.kapoor@hpe.com>
2022-01-03 15:32:37 +0530

    odim-5449: Update registry version to latest

--- lib-utilities/response/response.go
+++ lib-utilities/response/response.go
@@ -16,49 +16,49 @@ package response
 
 const (
 	// Created is the message for successful creation
-	Created = "Base.1.10.0.Created"
+	Created = "Base.1.11.0.Created"
 	// ExtendedInfo message
-	ExtendedInfo = "Base.1.10.0.ExtendedInfo"
+	ExtendedInfo = "Base.1.11.0.ExtendedInfo"
 	// AccountRemoved is the message for successful removal of account
-	AccountRemoved = "Base.1.10.0.AccountRemoved"
+	AccountRemoved = "Base.1.11.0.AccountRemoved"
 	// Success is the message for successful completion
-	Success = "Base.1.10.0.Success"
+	Success = "Base.1.11.0.Success"
 	// AccountModified is the message for successful account modification
-	AccountModified = "Base.1.10.0.AccountModified"
+	AccountModified = "Base.1.11.0.AccountModified"
 	// GeneralError defines the code at the time of General Error
-	GeneralError = "Base.1.10.0.GeneralError"
+	GeneralError = "Base.1.11.0.GeneralError"
 	// Failure code for failed message
-	Failure = "Base.1.10.0.Failed"
+	Failure = "Base.1.11.0.Failed"
 	// InsufficientPrivilege defines the status message at the time of Insufficient Privileges
-	InsufficientPrivilege = "Base.1.10.0.InsufficientPrivilege"
+	InsufficientPrivilege = "Base.1.11.0.InsufficientPrivilege"
 	// InternalError defines the status message at the time of Internal Error
-	InternalError = "Base.1.10.0.InternalError"
+	InternalError = "Base.1.11.0.InternalError"
 	// PropertyMissing defines the status message at the time of Property Missing
-	PropertyMissing = "Base.1.10.0.PropertyMissing"
+	PropertyMissing = "Base.1.11.0.PropertyMissing"
 	// PropertyUnknown defines the status message at the time of Property Unknown
-	PropertyUnknown = "Base.1.10.0.PropertyUnknown"
+	PropertyUnknown = "Base.1.11.0.PropertyUnknown"
 	// ResourceNotFound defines the status message at the time of Resource Not Found
-	ResourceNotFound = "Base.1.10.0.ResourceNotFound"
+	ResourceNotFound = "Base.1.11.0.ResourceNotFound"
 	// MalformedJSON defines the status message at the time of Malformed JSON
-	MalformedJSON = "Base.1.10.0.MalformedJSON"
+	MalformedJSON = "Base.1.11.0.MalformedJSON"
 	// PropertyValueNotInList defines the status message at the time of Property Value Not In List
-	PropertyValueNotInList = "Base.1.10.0.PropertyValueNotInList"
+	PropertyValueNotInList = "Base.1.11.0.PropertyValueNotInList"
 	// NoValidSession defines the status message at the time of No Valid Session
-	NoValidSession = "Base.1.10.0.NoValidSession"
+	NoValidSession = "Base.1.11.0.NoValidSession"
 	// ResourceInUse defines events aleady subscribed
-	ResourceInUse = "Base.1.10.0.ResourceInUse"
+	ResourceInUse = "Base.1.11.0.ResourceInUse"
 	// PropertyValueFormatError defines the status message  given the correct value type but the value of that property was not supported
-	PropertyValueFormatError = "Base.1.10.0.PropertyValueFormatError"
+	PropertyValueFormatError = "Base.1.11.0.PropertyValueFormatError"
 	// PropertyValueTypeError defines the message that the property is value given is having a different format
-	PropertyValueTypeError = "Base.1.10.0.PropertyValueTypeError"
+	PropertyValueTypeError = "Base.1.11.0.PropertyValueTypeError"
 	// ResourceAtURIUnauthorized defines the authorization failure with plugin or other resources
-	ResourceAtURIUnauthorized = "Base.1.10.0.ResourceAtUriUnauthorized"
+	ResourceAtURIUnauthorized = "Base.1.11.0.ResourceAtUriUnauthorized"
 	// CouldNotEstablishConnection defines the connection failure with plugin or other resources
-	CouldNotEstablishConnection = "Base.1.10.0.CouldNotEstablishConnection"
+	CouldNotEstablishConnection = "Base.1.11.0.CouldNotEstablishConnection"
 	// QueryCombinationInvalid defines the status message at the time of invalid query
-	QueryCombinationInvalid = "Base.1.10.0.QueryCombinationInvalid"
+	QueryCombinationInvalid = "Base.1.11.0.QueryCombinationInvalid"
 	// QueryNotSupported defines the status message at the time of not supported query
-	QueryNotSupported = "Base.1.10.0.QueryNotSupported"
+	QueryNotSupported = "Base.1.11.0.QueryNotSupported"
 	// ResourceRemoved is the message for successful removal of resource
 	ResourceRemoved = "ResourceEvent.1.0.3.ResourceRemoved"
 	// ResourceCreated is the message for successful creation of resource
@@ -66,17 +66,17 @@ const (
 	// TaskStarted is the message for denoting the starting of the task
 	TaskStarted = "TaskEvent.1.0.3.TaskStarted"
 	// ActionNotSupported defines requested POST operation is not supported by the resource
-	ActionNotSupported = "Base.1.10.0.ActionNotSupported"
+	ActionNotSupported = "Base.1.11.0.ActionNotSupported"
 	// ResourceAlreadyExists indicates the request is for creation of a resource, which already exists
-	ResourceAlreadyExists = "Base.1.10.0.ResourceAlreadyExists"
+	ResourceAlreadyExists = "Base.1.11.0.ResourceAlreadyExists"
 	// ActionParameterNotSupported indicates that the parameter supplied for the action is not supported on the resource.
-	ActionParameterNotSupported = "Base.1.10.0.ActionParameterNotSupported"
+	ActionParameterNotSupported = "Base.1.11.0.ActionParameterNotSupported"
 	// ResourceCannotBeDeleted indicates the requested delete operation cannot be performed
-	ResourceCannotBeDeleted = "Base.1.10.0.ResourceCannotBeDeleted"
+	ResourceCannotBeDeleted = "Base.1.11.0.ResourceCannotBeDeleted"
 	// PropertyValueConflict indicates that the requested write of a property value could not be completed, because of a conflict with another property value.
-	PropertyValueConflict = "Base.1.10.0.PropertyValueConflict"
+	PropertyValueConflict = "Base.1.11.0.PropertyValueConflict"
 	// NoOperation  defines the status message at the time of of there is no opeartion need to be performed.
-	NoOperation = "Base.1.10.0.NoOperation"
+	NoOperation = "Base.1.11.0.NoOperation"
 )
 
 // Response holds the generic response from odimra

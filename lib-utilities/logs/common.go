77609d1be2f4e486321d480ecbfee8e32d6c015e
lpooja <pooja.lakkundi@hpe.com>
2022-03-09 21:21:08 -0700

    ran go tools

--- lib-utilities/logs/common.go
+++ lib-utilities/logs/common.go
@@ -45,7 +45,7 @@ func getUserDetails(sessionToken string) (string, string) {
 	return sessionUserName, sessionRoleID
 }
 
-// maskRequestBody function
+// MaskRequestBody function
 // masking the request body, making password as null
 func MaskRequestBody(reqBody map[string]interface{}) string {
 	var jsonStr []byte

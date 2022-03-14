2c3e1e56ce0fa135fce4391c054d9854c230dc2e
Amar <amar2@hpe.com>
2022-03-14 18:14:22 +0530

    Added delete remote account route

--- svc-api/router/router.go
+++ svc-api/router/router.go
@@ -570,7 +570,7 @@ func Router() *iris.Application {
 	managers.Get("/{id}/RemoteAccountService/Accounts", manager.GetRemoteAccountService)
 	managers.Get("/{id}/RemoteAccountService/Accounts/{rid}", manager.GetRemoteAccountService)
 	managers.Post("/{id}/RemoteAccountService/Accounts", manager.CreateRemoteAccountService)
-	managers.Delete("/{id}/RemoteAccountService/Accounts", manager.DeleteRemoteAccountService)
+	managers.Delete("/{id}/RemoteAccountService/Accounts/{rid}", manager.DeleteRemoteAccountService)
 	managers.Get("/{id}/RemoteAccountService/Roles", manager.GetRemoteAccountService)
 	managers.Get("/{id}/RemoteAccountService/Roles/{rid}", manager.GetRemoteAccountService)
 	managers.Any("/{id}/RemoteAccountService", handle.ManagersMethodNotAllowed)

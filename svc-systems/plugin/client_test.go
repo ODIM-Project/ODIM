3c45eb26c6e3e27b792475d72c7b878ffee428e9
rahul chaube <rahul.chaube@hpe.com>
2022-05-19 12:12:02 +0530

    Updated Test Case svc-system/plugin

--- svc-systems/plugin/client_test.go
+++ svc-systems/plugin/client_test.go
@@ -17,13 +17,51 @@
 package plugin
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"testing"
 
+	"github.com/ODIM-Project/ODIM/lib-utilities/common"
 	"github.com/ODIM-Project/ODIM/lib-utilities/config"
-
+	"github.com/ODIM-Project/ODIM/lib-utilities/response"
+	"github.com/ODIM-Project/ODIM/svc-systems/smodel"
+	"github.com/ODIM-Project/ODIM/svc-systems/sresponse"
 	"github.com/stretchr/testify/assert"
 )
+
+func getEncryptedKey(t *testing.T, key []byte) []byte {
+
+	cryptedKey, err := common.EncryptWithPublicKey(key)
+	if err != nil {
+		t.Fatalf("error: failed to encrypt data: %v", err)
+	}
+	return cryptedKey
+}
+
+func mockPluginData(t *testing.T, pluginID, PreferredAuthType, port string) error {
+	password := getEncryptedKey(t, []byte("$2a$10$OgSUYvuYdI/7dLL5KkYNp.RCXISefftdj.MjbBTr6vWyNwAvht6ci"))
+	plugin := smodel.Plugin{
+		IP:                "localhost",
+		Port:              port,
+		Username:          "admin",
+		Password:          password,
+		ID:                pluginID,
+		PreferredAuthType: PreferredAuthType,
+	}
+	connPool, err := common.GetDBConnection(common.OnDisk)
+	if err != nil {
+		return fmt.Errorf("error while trying to connecting to DB: %v", err.Error())
+	}
+	if err = connPool.Create("Plugin", pluginID, plugin); err != nil {
+		return fmt.Errorf("error while trying to create new %v resource: %v", "Plugin", err.Error())
+	}
+	return nil
+}
 
 func Test_uriTranslator(t *testing.T) {
 
@@ -60,4 +98,230 @@ func Test_uriTranslator(t *testing.T) {
 			assert.Equal(t, test.expected, test.translate(test.toBeTranslated))
 		})
 	}
+}
+
+func Test_convertToString(t *testing.T) {
+	res := convertToString(125544)
+	assert.True(t, res == "125544", "There should be no error ")
+	JsonMarshalFunc = func(v interface{}) ([]byte, error) {
+		return nil, errors.New("")
+	}
+	res = convertToString("")
+	assert.NotNil(t, res, "There should be an error ")
+
+	JsonMarshalFunc = func(v interface{}) ([]byte, error) {
+		return json.Marshal(v)
+	}
+	status := is2xx(200)
+	assert.True(t, true, "there should be no error ", status)
+	status = is4xx(400)
+	assert.True(t, true, "there should be no error ", status)
+	status = hasToBeSkipped("Content-Length")
+	assert.True(t, true, "there should be no error ", status)
+}
+
+func Test_findAllPlugins(t *testing.T) {
+	config.SetUpMockConfig(t)
+	defer func() {
+		err := common.TruncateDB(common.InMemory)
+		if err != nil {
+			t.Fatalf("error: %v", err)
+		}
+		err = common.TruncateDB(common.OnDisk)
+		if err != nil {
+			t.Fatalf("error: %v", err)
+		}
+	}()
+	err := mockPluginData(t, "GRF", "XAuthToken", "9091")
+	if err != nil {
+		t.Fatalf("Error in creating mock PluginData :%v", err)
+	}
+
+	err = mockPluginData(t, "ILO", "BasicAuth", "9093")
+	if err != nil {
+		t.Fatalf("Error in creating mock PluginData :%v", err)
+	}
+
+	_, err = findAllPlugins("ILO")
+	assert.Nil(t, err, "There should be no error ")
+
+	JsonUnmarshalFunc = func(data []byte, v interface{}) error {
+		return errors.New("")
+	}
+	_, err = findAllPlugins("ILO")
+	assert.NotNil(t, err, "There should be an error ")
+
+	JsonUnmarshalFunc = func(data []byte, v interface{}) error {
+		return json.Unmarshal(data, v)
+	}
+	DecryptWithPrivateKeyFunc = func(ciphertext []byte) ([]byte, error) {
+		return nil, errors.New("")
+	}
+	_, err = findAllPlugins("ILO")
+	assert.NotNil(t, err, "There should be an error ")
+	DecryptWithPrivateKeyFunc = func(ciphertext []byte) ([]byte, error) {
+		return common.DecryptWithPrivateKey(ciphertext)
+	}
+	FindAllFunc = func(table, key string) ([][]byte, error) {
+		return nil, errors.New("")
+	}
+	_, err = findAllPlugins("ILO")
+	assert.NotNil(t, err, "There should be an error ")
+
+}
+
+func Test_createRPCResponse(t *testing.T) {
+	transform := func(s string) string {
+		return ""
+	}
+	rsp := createRPCResponse(200, http.Header{"content-type": []string{"application/json"}, "Content-Length": []string{"1025"}}, []byte(`{"username":"test"}`), transform)
+	assert.Equal(t, http.StatusOK, int(rsp.StatusCode), "There should not be error ")
+}
+
+func Test_client_extractResp(t *testing.T) {
+	c := &client{}
+
+	resp := c.extractResp(&http.Response{}, errors.New(""))
+	assert.Equal(t, http.StatusInternalServerError, int(resp.StatusCode), "Status should be StatusInternalServerError")
+
+	fbUserData := []byte("data")
+	resp = c.extractResp(&http.Response{StatusCode: 300, Body: ioutil.NopCloser(bytes.NewBufferString(string(fbUserData)))}, nil)
+	assert.Equal(t, http.StatusInternalServerError, int(resp.StatusCode), "Status should be StatusInternalServerError")
+	IoutilReadAllFunc = func(r io.Reader) ([]byte, error) {
+		return nil, errors.New("")
+	}
+	resp = c.extractResp(&http.Response{StatusCode: 300, Body: ioutil.NopCloser(bytes.NewBufferString(string(fbUserData)))}, nil)
+	assert.Equal(t, http.StatusInternalServerError, int(resp.StatusCode), "Status should be StatusInternalServerError")
+	IoutilReadAllFunc = func(r io.Reader) ([]byte, error) {
+		return ioutil.ReadAll(r)
+	}
+
+}
+
+func Test_client_Get(t *testing.T) {
+	client := client{
+		plugin: smodel.Plugin{
+			Port: "90093",
+			IP:   "localhost",
+		},
+		translator: &uriTranslator{&config.URLTranslation{
+			NorthBoundURL: map[string]string{
+				"ODIM": "redfish",
+			},
+			SouthBoundURL: map[string]string{
+				"redfish": "ODIM",
+			},
+		}},
+	}
+	resp := client.Get("/redis/v1/session")
+	assert.Equal(t, http.StatusInternalServerError, int(resp.StatusCode), "there should be error ")
+	resp = client.Delete("/redis/v1/session")
+	assert.Equal(t, http.StatusInternalServerError, int(resp.StatusCode), "there should be error ")
+
+	resp = client.Patch("/redis/v1/session", &json.RawMessage{})
+	assert.Equal(t, http.StatusInternalServerError, int(resp.StatusCode), "there should be error ")
+	resp = client.Post("/redis/v1/session", &json.RawMessage{})
+	assert.Equal(t, http.StatusInternalServerError, int(resp.StatusCode), "there should be error ")
+}
+
+func Test_multiTargetClient_Delete(t *testing.T) {
+	config := &config.URLTranslation{
+		NorthBoundURL: map[string]string{
+			"ODIM": "redfish",
+		},
+		SouthBoundURL: map[string]string{
+			"redfish": "ODIM",
+		},
+	}
+	client := func(plugin *smodel.Plugin) Client {
+		return &client{plugin: *plugin, translator: &uriTranslator{dictionaries: config}}
+	}
+	multiTargetClient := multiTargetClient{
+		createClient: client,
+		targets: []*smodel.Plugin{
+			{
+				IP:                "localhost",
+				Port:              "9093",
+				Username:          "admin",
+				Password:          []byte("asxasxszxzzZ"),
+				ID:                "ILO",
+				PreferredAuthType: "Basic-Auth",
+			},
+		},
+		call: ReturnFirstResponse(&CallConfig{}),
+	}
+	multiTargetClient.Get("redish/v1/session")
+	multiTargetClient.Post("redish/v1/session", &json.RawMessage{})
+	multiTargetClient.Patch("redish/v1/session", &json.RawMessage{})
+	multiTargetClient.Delete("redish/v1/session")
+}
+
+func TestNewClientFactory(t *testing.T) {
+
+	config := &config.URLTranslation{
+		NorthBoundURL: map[string]string{
+			"ODIM": "redfish",
+		},
+		SouthBoundURL: map[string]string{
+			"redfish": "ODIM",
+		},
+	}
+	fun := NewClientFactory(config)
+	client, _ := fun("redish/v1")
+	assert.Nil(t, client, "There should not error ")
+	client, _ = fun("*")
+	assert.Nil(t, client, "There should not error ")
+
+}
+
+func Test_returnFirst_Collect(t *testing.T) {
+
+	returnFirst := returnFirst{
+		ReqURI: "/redfish/v1/session",
+		resp:   &response.RPC{},
+	}
+	resp := response.RPC{}
+	res := returnFirst.Collect(resp)
+	assert.Nil(t, res, "There should be no error ")
+	res1 := returnFirst.GetResult()
+	assert.Equal(t, 0, int(res1.StatusCode), "There should be no error ")
+	AggregateResults(&CallConfig{})
+	res2 := ReturnFirstResponse(&CallConfig{})
+	assert.NotNil(t, res2, "There should be an error ")
+
+	returnFirst.resp = nil
+	res1 = returnFirst.GetResult()
+	assert.Equal(t, http.StatusNotFound, int(res1.StatusCode), "Status code should be StatusNotFound")
+
+	collectCollectionMembers := collectCollectionMembers{
+		collection: sresponse.Collection{},
+		errored:    false,
+	}
+	collectionRes := collectCollectionMembers.Collect(response.RPC{})
+	assert.NotNil(t, collectionRes, "There should be error ")
+
+	JsonUnmarshalFunc = func(data []byte, v interface{}) error {
+		return fmt.Errorf("")
+	}
+	collectionRes = collectCollectionMembers.Collect(response.RPC{StatusCode: 200, Body: []byte(`{"Members":[{"@@odata.id":"/redish/v1/session/1"}]}`)})
+	assert.NotNil(t, collectionRes, "There should be error ")
+
+	JsonUnmarshalFunc = func(data []byte, v interface{}) error {
+		return json.Unmarshal(data, v)
+	}
+	collectionRes = collectCollectionMembers.Collect(response.RPC{StatusCode: 200, Body: []byte(`{"Members":[{"@@odata.id":"/redish/v1/session/1"}]}`)})
+	assert.Nil(t, collectionRes, "There should be no error ")
+
+	resultRes := collectCollectionMembers.GetResult()
+	assert.NotNil(t, resultRes, "There should be error ")
+	JsonMarshalFunc = func(v interface{}) ([]byte, error) {
+		return nil, errors.New("")
+	}
+	res3 := collectCollectionMembers.GetResult()
+	assert.NotNil(t, res3, "There should be error ")
+	JsonMarshalFunc = func(v interface{}) ([]byte, error) {
+		return json.Marshal(v)
+	}
+	chassisRes := getChassisID("redis/v1")
+	assert.NotNil(t, chassisRes, "There should be no error ")
 }

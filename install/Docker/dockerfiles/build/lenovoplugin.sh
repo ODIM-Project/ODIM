9b642602da9112edb50e4390292116872dfd7577
rohitkumar-ro <rohitk@hpe.com>
2022-05-26 17:46:39 +0000

    ODIM-4563: renaming the folder name from run to build

--- install/Docker/dockerfiles/build/lenovoplugin.sh
+++ install/Docker/dockerfiles/build/lenovoplugin.sh
@@ -12,8 +12,22 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+protos=("account" "aggregator" "auth" "chassis" "events" "fabrics" "managers" "role" "session" "systems" "task" "telemetry" "update" "compositionservice" "licenses")
+for str in ${protos[@]}; do
+  proto_path="$(pwd)/lib-utilities/proto/$str"
+  proto_file_name="$str.proto"
+  if [ $str == 'auth' ]
+  then
+    proto_file_name="odim_auth.proto"
+  fi
+  if [ $str == 'compositionservice' ]
+  then
+    proto_file_name="composition_service.proto"
+  fi
+  protoc --go_opt=M$proto_file_name=./ --go_out=plugins=grpc:$proto_path --proto_path=$proto_path $proto_file_name
+done
 
-LIST=("plugin-redfish")
+LIST=("plugin-lenovo")
 echo $LIST
 for i in $LIST; do
     cd $i

ba07e228547c725753f0765f0acf9780cd2cb762
rahul chaube <rahul.chaube@hpe.com>
2022-08-02 11:32:47 +0530

    update string seprators

--- svc-aggregation/agmodel/system.go
+++ svc-aggregation/agmodel/system.go
@@ -1181,7 +1181,7 @@ func AddNewHostToAggregateHostIndex(aggregateID string, hostIP string) error {
 	if err != nil {
 		return err1
 	}
-	devSub := strings.Split(aggreagtes[0], "::")
+	devSub := strings.Split(aggreagtes[0], "||")
 	ips := getSliceFromString(devSub[1])
 	isUpdate := true
 	for _, ip := range ips {
@@ -1210,7 +1210,7 @@ func RemoveNewIPToAggregateHostIndex(aggregateID string, hostIP string) error {
 	if err != nil {
 		return err1
 	}
-	devSub := strings.Split(aggreagtes[0], "::")
+	devSub := strings.Split(aggreagtes[0], "||")
 	ips := getSliceFromString(devSub[1])
 	ips = removeIps(ips, hostIP)
 	err1 = conn.UpdateAggregateHosts(aggregateHostIndex, aggregateID, ips)

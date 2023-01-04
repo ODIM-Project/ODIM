3aa5c236e573e59a7fc7cea6d2f72c3496d9d7b0
Bharath Kumar Bhaskar Kizhur <68339187+Bharath-KKB@users.noreply.github.com>
2023-01-05 01:02:00 +0530

    Update events.go
    Refined comments for EventType

--- lib-dmtf/model/events.go
+++ lib-dmtf/model/events.go
@@ -14,13 +14,20 @@
 
 package model
 
-// EventType - This property shall indicate the type of event.
-// "This property has been deprecated.  Starting with Redfish Specification v1.6 (Event v1.3),
-// subscriptions are based on the RegistryPrefix and ResourceType properties and not on the EventType property."
+// EventType - This property shall contain an array that contains the types 
+// of events that shall be sent to the destination.  To specify that a client
+// is subscribing for Metric Reports, the EventTypes property should 
+// include 'MetricReport'.  If the subscription does not include this property, 
+//the service shall use a single element with a default of `Other`.
+// Note: This property has been deprecated.  Starting with Redfish Specification 
+// v1.6 (Event v1.3), subscriptions are based on the RegistryPrefix and ResourceType 
+// properties and not on the EventType property.  Use EventFormatType to create 
+// subscriptions for Metric Reports.  
 type EventType string
 
-// MessageSeverity - The severity of the message in this event. This property shall contain the severity of the message in this event.
-// Services can replace the value defined in the message registry with a value more applicable to the implementation.
+// MessageSeverity - The severity of the message in this event. This property shall 
+// contain the severity of the message in this event. Services can replace the value
+// defined in the message registry with a value more applicable to the implementation.
 type MessageSeverity string
 
 //The subscription type for events.
@@ -29,24 +36,32 @@ type SubscriptionType string
 const (
 	// EventTypeAlert - "Alert": "A condition requires attention."
 	EventTypeAlert EventType = "Alert"
+	
 	// EventTypeMetricReport - "MetricReport": "The telemetry service is sending a metric report."
 	EventTypeMetricReport EventType = "MetricReport"
+	
 	// EventTypeOther - "Other": "Because EventType is deprecated as of Redfish Specification v1.6,
 	// the event is based on a registry or resource but not an EventType."
 	EventTypeOther EventType = "Other"
+	
 	// EventTypeResourceAdded - "ResourceAdded": "A resource has been added."
 	EventTypeResourceAdded EventType = "ResourceAdded"
+	
 	// EventTypeResourceRemoved - "ResourceRemoved": "A resource has been removed."
 	EventTypeResourceRemoved EventType = "ResourceRemoved"
+	
 	// EventTypeResourceUpdated - "ResourceUpdated": "A resource has been updated."
 	EventTypeResourceUpdated EventType = "ResourceUpdated"
+	
 	// EventTypeStatusChange - "StatusChange": "The status of a resource has changed."
 	EventTypeStatusChange EventType = "StatusChange"
 
 	// MessageSeverityCritical - "Critical": "A critical condition requires immediate attention."
 	MessageSeverityCritical MessageSeverity = "Critical"
+	
 	// MessageSeverityOK - "OK": "Normal."
 	MessageSeverityOK MessageSeverity = "OK"
+	
 	// MessageSeverityWarning - "Warning": "A condition requires attention."
 	MessageSeverityWarning MessageSeverity = "Warning"
 	

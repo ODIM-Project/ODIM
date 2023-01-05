4f85a7418c2903defcafc3dd4e0d99bb9ebf7da1
hemkapoor <hemant.kapoor@hpe.com>
2023-01-05 08:48:26 +0000

    odim-6894: Cleanup logs for messagebus and persistence lib

--- lib-persistence-manager/persistencemgr/redis_test.go
+++ lib-persistence-manager/persistencemgr/redis_test.go
@@ -1,15 +1,15 @@
-//(C) Copyright [2020] Hewlett Packard Enterprise Development LP
+// (C) Copyright [2020] Hewlett Packard Enterprise Development LP
 //
-//Licensed under the Apache License, Version 2.0 (the "License"); you may
-//not use this file except in compliance with the License. You may obtain
-//a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
-//Unless required by applicable law or agreed to in writing, software
-//distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-//WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-//License for the specific language governing permissions and limitations
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
 // under the License.
 package persistencemgr
 
@@ -1165,7 +1165,7 @@ func TestGetCurrentMasterHostPort(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := GetCurrentMasterHostPort(tt.args.dbConfig)
+			got, got1, _ := GetCurrentMasterHostPort(tt.args.dbConfig)
 			if got != tt.want {
 				t.Errorf("GetCurrentMasterHostPort() got = %v, want %v", got, tt.want)
 			}

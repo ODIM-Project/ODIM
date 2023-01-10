46a711fd939c7c9c58d858f123c873cb14bac14c
mohamed-shahid-afrd <mohamed-shahid.afrid@hpe.com>
2023-01-10 10:01:55 +0530

    Remove the duplicate Struct action

--- lib-dmtf/model/events.go
+++ lib-dmtf/model/events.go
@@ -14,18 +14,18 @@
 
 package model
 
-// EventType - This property shall contain an array that contains the types 
+// EventType - This property shall contain an array that contains the types
 // of events that shall be sent to the destination.  To specify that a client
-// is subscribing for Metric Reports, the EventTypes property should 
-// include 'MetricReport'.  If the subscription does not include this property, 
+// is subscribing for Metric Reports, the EventTypes property should
+// include 'MetricReport'.  If the subscription does not include this property,
 //the service shall use a single element with a default of `Other`.
-// Note: This property has been deprecated.  Starting with Redfish Specification 
-// v1.6 (Event v1.3), subscriptions are based on the RegistryPrefix and ResourceType 
-// properties and not on the EventType property.  Use EventFormatType to create 
-// subscriptions for Metric Reports.  
+// Note: This property has been deprecated.  Starting with Redfish Specification
+// v1.6 (Event v1.3), subscriptions are based on the RegistryPrefix and ResourceType
+// properties and not on the EventType property.  Use EventFormatType to create
+// subscriptions for Metric Reports.
 type EventType string
 
-// MessageSeverity - The severity of the message in this event. This property shall 
+// MessageSeverity - The severity of the message in this event. This property shall
 // contain the severity of the message in this event. Services can replace the value
 // defined in the message registry with a value more applicable to the implementation.
 type MessageSeverity string
@@ -39,152 +39,152 @@ type SNMPEncryptionProtocols string
 // The subscription type for events.
 type SubscriptionType string
 
-// This property shall contain the types of programs that can log messages.  
-// If this property contains an empty array or is absent, all facilities 
+// This property shall contain the types of programs that can log messages.
+// If this property contains an empty array or is absent, all facilities
 // shall be indicated. Facility values are described in the RFC5424.
 type SyslogFacility string
 
-// This property shall contain the lowest syslog severity level 
-// that will be forwarded. The service shall forward all messages 
-// equal to or greater than the value in this property.  The value 
+// This property shall contain the lowest syslog severity level
+// that will be forwarded. The service shall forward all messages
+// equal to or greater than the value in this property.  The value
 // `All` shall indicate all severities.
 type SyslogSeverity string
 
 const (
 	// EventTypeAlert - "Alert": "A condition requires attention."
 	EventTypeAlert EventType = "Alert"
-	
+
 	// EventTypeMetricReport - "MetricReport": "The telemetry service is sending a metric report."
 	EventTypeMetricReport EventType = "MetricReport"
-	
+
 	// EventTypeOther - "Other": "Because EventType is deprecated as of Redfish Specification v1.6,
 	// the event is based on a registry or resource but not an EventType."
 	EventTypeOther EventType = "Other"
-	
+
 	// EventTypeResourceAdded - "ResourceAdded": "A resource has been added."
 	EventTypeResourceAdded EventType = "ResourceAdded"
-	
+
 	// EventTypeResourceRemoved - "ResourceRemoved": "A resource has been removed."
 	EventTypeResourceRemoved EventType = "ResourceRemoved"
-	
+
 	// EventTypeResourceUpdated - "ResourceUpdated": "A resource has been updated."
 	EventTypeResourceUpdated EventType = "ResourceUpdated"
-	
+
 	// EventTypeStatusChange - "StatusChange": "The status of a resource has changed."
 	EventTypeStatusChange EventType = "StatusChange"
 
 	// MessageSeverityCritical - "Critical": "A critical condition requires immediate attention."
 	MessageSeverityCritical MessageSeverity = "Critical"
-	
+
 	// MessageSeverityOK - "OK": "Normal."
 	MessageSeverityOK MessageSeverity = "OK"
-	
+
 	// MessageSeverityWarning - "Warning": "A condition requires attention."
 	MessageSeverityWarning MessageSeverity = "Warning"
-	
+
 	// authentication using SNMP community strings and the value of TrapCommunity.
 	SNMPAuthenticationProtocolsCommunityString SNMPAuthenticationProtocols = "CommunityString"
-	
+
 	// authentication for SNMPv3 access conforms to the RFC7860-defined usmHMAC128SHA224AuthProtocol.
 	SNMPAuthenticationProtocolsHMAC128_SHA224 SNMPAuthenticationProtocols = "HMAC128_SHA224"
-	
+
 	// authentication for SNMPv3 access conforms to the RFC7860-defined usmHMAC192SHA256AuthProtocol.
 	SNMPAuthenticationProtocolsHMAC192_SHA256 SNMPAuthenticationProtocols = "HMAC192_SHA256"
-	
+
 	// authentication for SNMPv3 access conforms to the RFC7860-defined usmHMAC256SHA384AuthProtocol.
 	SNMPAuthenticationProtocolsHMAC256_SHA384 SNMPAuthenticationProtocols = "HMAC256_SHA384"
-	
+
 	// authentication for SNMPv3 access conforms to the RFC7860-defined usmHMAC384SHA512AuthProtocol.
 	SNMPAuthenticationProtocolsHMAC384_SHA512 SNMPAuthenticationProtocols = "HMAC384_SHA512"
-	
+
 	// authentication conforms to the RFC1321-defined HMAC-MD5-96 authentication protocol.
 	SNMPAuthenticationProtocolsHMAC_MD5 SNMPAuthenticationProtocols = "HMAC_MD5"
-	
+
 	// authentication conforms to the RFC3414-defined HMAC-SHA-96 authentication protocol.
 	SNMPAuthenticationProtocolsHMAC_SHA96 SNMPAuthenticationProtocols = "HMAC_SHA96"
-	
+
 	// authentication is not required.
 	SNMPAuthenticationProtocolsNone SNMPAuthenticationProtocols = "None"
-	
+
 	// encryption conforms to the RFC3414-defined CBC-DES encryption protocol.
 	SNMPEncryptionProtocolsCBC_DES SNMPEncryptionProtocols = "CBC_DES"
-	
+
 	// encryption conforms to the RFC3414-defined CFB128-AES-128 encryption protocol.
 	SNMPEncryptionProtocolsCFB128_AES128 SNMPEncryptionProtocols = "CFB128_AES128"
-	
+
 	// there is no encryption.
 	SNMPEncryptionProtocolsNone SNMPEncryptionProtocols = "None"
-	
-	// Subscription Types for events. Currently ODIM only support subscriptions 
+
+	// Subscription Types for events. Currently ODIM only support subscriptions
 	// of type RedFishTypeEvent.
-	// SubscriptionTypeRedFishEvent - The subscription follows the Redfish 
+	// SubscriptionTypeRedFishEvent - The subscription follows the Redfish
 	// Specification for event notifications. To send an event notification,
 	// a service sends an HTTP POST to the subscriber's destination URI.
 	SubscriptionTypeRedFishEvent SubscriptionType = "RedfishEvent"
-        
-	// SubscriptionTySubscriptionTypeSSE - The subscription follows the HTML5 
+
+	// SubscriptionTySubscriptionTypeSSE - The subscription follows the HTML5
 	// server-sent event definition for event notifications.
 	SubscriptionTySubscriptionTypeSSE SubscriptionType = "SSE"
-	
-        // SubscriptionTypeSNMPTrap - The subscription follows the various versions
+
+	// SubscriptionTypeSNMPTrap - The subscription follows the various versions
 	// of SNMP Traps for event notifications.
 	SubscriptionTypeSNMPTrap SubscriptionType = "SNMPTrap"
-	
-	// SubscriptionTypeSNMPInform - The subscription follows versions 2 and 3 of 
+
+	// SubscriptionTypeSNMPInform - The subscription follows versions 2 and 3 of
 	// SNMP Inform for event notifications.
-        SubscriptionTypeSNMPInform SubscriptionType = "SNMPInform"
-	
-	// SubscriptionTypeSyslog - The subscription sends Syslog messages for 
+	SubscriptionTypeSNMPInform SubscriptionType = "SNMPInform"
+
+	// SubscriptionTypeSyslog - The subscription sends Syslog messages for
 	// event notifications.
-        SubscriptionTypeSyslog SubscriptionType = "Syslog"
-	
+	SubscriptionTypeSyslog SubscriptionType = "Syslog"
+
 	// SubscriptionTypeOEM - The subscription is an OEM subscription.
-        SubscriptionTypeOEM SubscriptionType = "OEM"
+	SubscriptionTypeOEM SubscriptionType = "OEM"
 
 	// Security/authentication messages.
-	SyslogFacilityAuth SyslogFacility = "Auth" 
+	SyslogFacilityAuth SyslogFacility = "Auth"
 
 	// Security/authentication messages.
-	SyslogFacilityAuthpriv SyslogFacility = "Authpriv" 
+	SyslogFacilityAuthpriv SyslogFacility = "Authpriv"
 
 	//Log alert.
 	SyslogFacilityConsole SyslogFacility = "Console"
 
-	//Clock daemon. 
+	//Clock daemon.
 	SyslogFacilityCron SyslogFacility = "Cron"
- 
+
 	//System daemons.
 	SyslogFacilityDaemon SyslogFacility = "Daemon"
 
 	//FTP daemon.
-	SyslogFacilityFTP SyslogFacility = "FTP" 
- 
-	//Kernel messages.        
-	SyslogFacilityKern SyslogFacility = "Kern" 
-                
-	//Line printer subsystem.
-	SyslogFacilityLPR SyslogFacility = "LPR" 
+	SyslogFacilityFTP SyslogFacility = "FTP"
 
-	//Locally used facility 0.                
-	SyslogFacilityLocal0 SyslogFacility = "Local0" 
+	//Kernel messages.
+	SyslogFacilityKern SyslogFacility = "Kern"
+
+	//Line printer subsystem.
+	SyslogFacilityLPR SyslogFacility = "LPR"
+
+	//Locally used facility 0.
+	SyslogFacilityLocal0 SyslogFacility = "Local0"
 
 	//Locally used facility 1.
 	SyslogFacilityLocal1 SyslogFacility = "Local1"
 
 	//Locally used facility 2.
-	SyslogFacilityLocal2 SyslogFacility = "Local2" 
+	SyslogFacilityLocal2 SyslogFacility = "Local2"
 
 	//Locally used facility 3.
 	SyslogFacilityLocal3 SyslogFacility = "Local3"
 
 	//Locally used facility 4.
-	SyslogFacilityLocal4 SyslogFacility = "Local4" 
+	SyslogFacilityLocal4 SyslogFacility = "Local4"
 
 	//Locally used facility 5.
-	SyslogFacilityLocal5 SyslogFacility = "Local5" 
+	SyslogFacilityLocal5 SyslogFacility = "Local5"
 
 	//Locally used facility 6.
-	SyslogFacilityLocal6 SyslogFacility = "Local6" 
+	SyslogFacilityLocal6 SyslogFacility = "Local6"
 
 	//Locally used facility 7.
 	SyslogFacilityLocal7 SyslogFacility = "Local7"
@@ -199,13 +199,13 @@ const (
 	SyslogFacilityNews SyslogFacility = "News"
 
 	//Log audit.
-	SyslogFacilitySecurity SyslogFacility = "Security" 
+	SyslogFacilitySecurity SyslogFacility = "Security"
 
 	//Scheduling daemon.
-	SyslogFacilitySolarisCron SyslogFacility = "SolarisCron" 
+	SyslogFacilitySolarisCron SyslogFacility = "SolarisCron"
 
 	//Messages generated internally by syslogd.
-	SyslogFacilitySyslog SyslogFacility = "Syslog" 
+	SyslogFacilitySyslog SyslogFacility = "Syslog"
 
 	//UUCP subsystem.
 	SyslogFacilityUUCP SyslogFacility = "UUCP"
@@ -241,9 +241,9 @@ const (
 	SyslogSeverityWarning SyslogSeverity = "Warning"
 )
 
-// Event schema describes the JSON payload received by an event destination, which has 
-// subscribed to event notification, when events occur. This resource contains data 
-// about events, including descriptions, severity, and a message identifier to a 
+// Event schema describes the JSON payload received by an event destination, which has
+// subscribed to event notification, when events occur. This resource contains data
+// about events, including descriptions, severity, and a message identifier to a
 // message registry that can be accessed for further information.
 // Refer to Event.v1_7_0.json of the redfish spec for more details
 type Event struct {
@@ -259,8 +259,8 @@ type Event struct {
 	Oem          interface{}   `json:"Oem,omitempty"`
 }
 
-// EventRecord - a single event in the Events array of the Event Resource. This  has a 
-// set of properties that describe a single event. Because Events is an array, more than 
+// EventRecord - a single event in the Events array of the Event Resource. This  has a
+// set of properties that describe a single event. Because Events is an array, more than
 // one EventRecord can be sent simultaneously.
 // Refer to Event.v1_7_0.json of the redfish spec for more details
 type EventRecord struct {
@@ -327,10 +327,6 @@ type EventDestination struct {
 	VerifyCertificate            bool             `json:"VerifyCertificate,omitempty"`
 }
 
-// place holders for attribs needed in EventDestination.
-type Actions struct{}
-
-
 // This type shall contain the settings for an SNMP event destination.
 // Reference	                : EventDestination.v1_12_0.json
 type SNMPSettings struct {
@@ -343,10 +339,10 @@ type SNMPSettings struct {
 	TrapCommunity          string                      `json:"TrapCommunity,omitempty"`
 }
 
-// This action shall suspend an event subscription.  No events shall be 
-// sent to the event destination until invocation of the ResumeSubscription 
-// action.  The value of the State property within Status shall contain 
-// `Disabled` for a suspended subscription.  The service may buffer 
+// This action shall suspend an event subscription.  No events shall be
+// sent to the event destination until invocation of the ResumeSubscription
+// action.  The value of the State property within Status shall contain
+// `Disabled` for a suspended subscription.  The service may buffer
 // events while the subscription is suspended.
 // Reference	                : EventDestination.v1_12_0.json
 type SuspendSubscription struct {
@@ -362,9 +358,3 @@ type SyslogFilter struct {
 	LogFacilities  SyslogFacility `json:"LogFacilities,omitEmpty"`
 	LowestSeverity SyslogSeverity `json:"LogFacilities,omitEmpty"`
 }
-
-
-
-
-
-

cc5499657e2b7ee02462daba957eb5fc66075abb
ashraf-vazeer <ashraf.vazeer@hpe.com>
2023-01-31 11:05:14 +0530

    added nginx. 3.6 pause version and pigz pkg

--- odim-controller/scripts/odim-controller.py
+++ odim-controller/scripts/odim-controller.py
@@ -496,6 +496,8 @@ def scale_in_k8s():
 
 	if not DRY_RUN_SET:
 		load_password_from_vault(cur_dir)
+		logger.info("Removing the k8s images")
+		delete_k8_images(K8S_INVENTORY_FILE,nodes_list)
 		logger.info("Starting k8s cluster scale-in")
 		k8s_rm_cmd = 'ansible-playbook -i {host_conf_file} --become --become-user=root --extra-vars "node={rm_node_list}" -e delete_nodes_confirmation=yes remove-node.yml'.format(host_conf_file=K8S_INVENTORY_FILE, rm_node_list=nodes_list)
 		ret = exec(k8s_rm_cmd, {'ANSIBLE_BECOME_PASS': ANSIBLE_BECOME_PASS})
@@ -522,7 +524,7 @@ def scale_in_k8s():
 		else:
 			logger.info("Post-uninstall action was successful on nodes %s", nodes_list)
 
-		delete_k8_images(K8S_INVENTORY_FILE,nodes_list)
+		#delete_k8_images(K8S_INVENTORY_FILE,nodes_list)
 		# remove copy of controller config file created
 		os.remove(helm_config_file)
 		os.remove(odimra_config_file)

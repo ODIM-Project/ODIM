00242da755ad24630e805af20d16bdca04d308ef
mohamed-shahid-afrd <mohamed-shahid.afrid@hpe.com>
2023-02-20 13:23:13 +0530

    Making subscription of Kafka topic sequential

--- svc-events/main.go
+++ svc-events/main.go
@@ -1,15 +1,15 @@
-//(C) Copyright [2020] Hewlett Packard Enterprise Development LP
+// (C) Copyright [2020] Hewlett Packard Enterprise Development LP
 //
-//Licensed under the Apache License, Version 2.0 (the "License"); you may
-//not use this file except in compliance with the License. You may obtain
-//a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
-//Unless required by applicable law or agreed to in writing, software
-//distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-//WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-//License for the specific language governing permissions and limitations
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
 // under the License.
 package main
 
@@ -82,6 +82,27 @@ func main() {
 	events := rpc.GetPluginContactInitializer()
 	eventsproto.RegisterEventsServer(services.ODIMService.Server(), events)
 
+	evcommon.ConfigFilePath = os.Getenv("CONFIG_FILE_PATH")
+	if evcommon.ConfigFilePath == "" {
+		log.Fatal("error: no value get the environment variable CONFIG_FILE_PATH")
+	}
+
+	// TrackConfigFileChanges monitors the odim config changes using fsnotfiy
+	go evcommon.TrackConfigFileChanges(errChan)
+
+	// Subscribe to intercomm messagebus queue
+	go func() {
+		consumer.SubscribeCtrlMsgQueue(config.Data.MessageBusConf.OdimControlMessageQueue)
+		startUPInterface := evcommon.StartUpInteraface{
+			DecryptPassword: common.DecryptWithPrivateKey,
+			EMBConsume:      consumer.Consume,
+		}
+		startUPInterface.SubscribePluginEMB()
+	}()
+	//consumer.SubscribeCtrlMsgQueue(config.Data.MessageBusConf.OdimControlMessageQueue)
+
+	// Subscribe to EMBs of all the available plugins
+
 	// CreateJobQueue defines the queue which will act as an infinite buffer
 	// In channel is an entry or input channel and the Out channel is an exit or output channel
 	jobQueueSize := 10
@@ -97,24 +118,6 @@ func main() {
 	// RunReadWorkers will create a worker pool for doing a specific task
 	// which is passed to it as ProcessCtrlMsg method after reading the data from the channel.
 	common.RunReadWorkers(consumer.CtrlMsgProcQueue, evcommon.ProcessCtrlMsg, 1)
-
-	evcommon.ConfigFilePath = os.Getenv("CONFIG_FILE_PATH")
-	if evcommon.ConfigFilePath == "" {
-		log.Fatal("error: no value get the environment variable CONFIG_FILE_PATH")
-	}
-
-	// TrackConfigFileChanges monitors the odim config changes using fsnotfiy
-	go evcommon.TrackConfigFileChanges(errChan)
-
-	// Subscribe to intercomm messagebus queue
-	go consumer.SubscribeCtrlMsgQueue(config.Data.MessageBusConf.OdimControlMessageQueue)
-
-	// Subscribe to EMBs of all the available plugins
-	startUPInterface := evcommon.StartUpInteraface{
-		DecryptPassword: common.DecryptWithPrivateKey,
-		EMBConsume:      consumer.Consume,
-	}
-	go startUPInterface.SubscribePluginEMB()
 
 	// Run server
 	if err := services.ODIMService.Run(); err != nil {

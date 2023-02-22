234899f45384a1ce3c64374c51f0e64893debbb1
mohamed-shahid-afrd <mohamed-shahid.afrid@hpe.com>
2023-02-22 12:10:08 +0530

    Task api for debug profile

--- svc-task/main.go
+++ svc-task/main.go
@@ -79,7 +79,7 @@ func main() {
 		log.Fatal("error: no value get the environment variable CONFIG_FILE_PATH")
 	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/custom_debug_path/profile", pprof.Profile)
+	mux.HandleFunc("/debug/profile", pprof.Profile)
 	go func() {
 		log.Fatal(http.ListenAndServe(":7777", mux))
 	}()

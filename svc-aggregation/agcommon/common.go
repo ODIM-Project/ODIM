5756b6c643a4d7ffe595a170d09be3d0c4328ada
kavyamani596 <101591350+kavyamani596@users.noreply.github.com>
2023-03-09 17:08:23 +0530

    constants

--- svc-aggregation/agcommon/common.go
+++ svc-aggregation/agcommon/common.go
@@ -123,8 +123,8 @@ func GetStorageResources(ctx context.Context, oid string) map[string]interface{}
 func (e *DBInterface) AddConnectionMethods(connectionMethodConf []config.ConnectionMethodConf) error {
 	aggTransactionID := uuid.New()
 	podName := os.Getenv("POD_NAME")
-	actionID := common.Actions[common.ActionKey{Service: "AggregationService", Uri: "ConnectionMethods", Method: "GET"}].ActionID
-	actionName := common.Actions[common.ActionKey{Service: "AggregationService", Uri: "ConnectionMethods", Method: "GET"}].ActionName
+	actionID := common.Actions[common.ActionKey{Service: "AddConnectionMethods", Uri: "ConnectionMethod", Method: "POST-TO-DB"}].ActionID
+	actionName := common.Actions[common.ActionKey{Service: "AddConnectionMethods", Uri: "ConnectionMethod", Method: "POST-TO-DB"}].ActionName
 	ctx := CreateContext(aggTransactionID.String(), actionID, actionName, "1", common.AggregationService, podName)
 	connectionMethodsKeys, err := e.GetAllKeysFromTableInterface(ctx, "ConnectionMethod")
 	if err != nil {

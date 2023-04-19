0b0b57e31ec12fa46c2ec101b1ad1bce465a1ca1
Mohamed Shahid Afrid <108655688+mohamed-shahid-afrid@users.noreply.github.com>
2023-04-20 02:15:39 +0530

    Revert "Replacing nohup with tee to print the logs on console too "

--- install/Docker/dockerfiles/scripts/start_grfplugin.sh
+++ install/Docker/dockerfiles/scripts/start_grfplugin.sh
@@ -41,7 +41,7 @@ run_forever()
 start_grfplugin()
 {
 	export PLUGIN_CONFIG_FILE_PATH=/etc/grfplugin_config/config.json
-	/bin/plugin-redfish 2>&1 | tee /var/log/grfplugin_logs/grfplugin.log 
+	nohup /bin/plugin-redfish >> /var/log/grfplugin_logs/grfplugin.log 2>&1 &
 	PID=$!
 	sleep 3
 

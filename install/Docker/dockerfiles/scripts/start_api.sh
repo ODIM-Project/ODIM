cf80000ec0cbe40cf67470fbef40e3818192eda4
mohamed-shahid-afrd <mohamed-shahid.afrid@hpe.com>
2023-05-05 15:36:59 +0530

    Check on the flag

--- install/Docker/dockerfiles/scripts/start_api.sh
+++ install/Docker/dockerfiles/scripts/start_api.sh
@@ -45,6 +45,7 @@ start_api()
 	logs_on_console=$(cat $CONFIG_FILE_PATH | grep logsRedirectionToConsole| cut -d : -f2 | cut -d , -f1 | tr -d " " )
     if [[ $logs_on_console == "true" ]];
     then
+	echo "printing logs to console"
     client_request_timeout=$(echo $(cat $CONFIG_FILE_PATH | grep SouthBoundRequestTimeoutInSecs | cut -d : -f2 | cut -d , -f1 | tr -d " " )s)
 	/bin/svc-api --registry=etcd --registry_address=${registry_address} --client_request_timeout=${client_request_timeout} 2>&1 &
 	else

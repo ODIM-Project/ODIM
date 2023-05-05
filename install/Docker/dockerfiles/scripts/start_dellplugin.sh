2704e381cf7d6fb507c593b6e64b34c702aa52ba
mohamed-shahid-afrd <mohamed-shahid.afrid@hpe.com>
2023-05-05 16:10:39 +0530

    testing the checks

--- install/Docker/dockerfiles/scripts/start_dellplugin.sh
+++ install/Docker/dockerfiles/scripts/start_dellplugin.sh
@@ -42,7 +42,8 @@ start_dellplugin()
 {
 	export PLUGIN_CONFIG_FILE_PATH=/etc/dellplugin_config/config.json
 	logs_on_console=$(cat $CONFIG_FILE_PATH | grep logsRedirectionToConsole| cut -d : -f2 | cut -d , -f1 | tr -d " " )
-    if [[ $logs_on_console == "true" ]];
+    echo $logs_on_console
+	if [[ $logs_on_console == "true" ]];
 	echo "printing logs to console"
     then
 	/bin/plugin-dell 2>&1 &
